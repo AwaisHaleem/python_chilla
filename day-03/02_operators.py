f89cc45f34c3ae44cabbb92fceb5f726ad46d21e
AwaisHaleem <awaishaleem20@gmail.com>
2023-09-25 17:25:34 +0500

    Started python ka chilla and completed first two days of work

---
+++ day-03/02_operators.py
@@ -0,0 +1,22 @@
+# Addition
+print(4 + 5)
+
+# Subtraction
+print(4 - 5)
+
+# Multiplication
+print(4 * 5)
+
+# Division
+print(4 / 5)
+
+# Exponentiation
+print(4 ** 5)
+
+# Floor division
+print(4 // 5)
+
+# Modulus
+print(4 % 5)
+
+
---
+++ day-03/03_strings.py
@@ -0,0 +1,18 @@
+
+print("Here's a single-line string.")
+print("Here's another single-line string with a special character: \t")
+print("And here's a string with a variable inside: {}".format("my_variable"))
+
+
+print("""
+This is a multiline string.
+It can span multiple lines,
+and can contain any kind of text.
+""")
+
+print("This is another multiline string, but it's a bit shorter.")
+print("The meaning of life, the universe, and everything is 42.")
+print("A wise man once said, 'The only thing constant is change.'")
+print("To be or not to be, that is the question.")
+print("I love the smell of napalm in the morning.")
+print("May the Force be with you.")
